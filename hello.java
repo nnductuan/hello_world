2da12779584493e0a0834b52bd9e2ee8b7200e38
Tuan <nnductuan@gmail.com>
2017-10-18 11:32:17 +0700

    test jenkins
    Signed-off-by: Tuan <nnductuan@gmail.com>

--- hello.java
+++ hello.java
@@ -1,8 +1,5 @@
 public class hello {
     public static void main(String[] args) {
         System.out.println("Hello, World");
-        System.out.println("Hello, World");
-        System.out.println("Bye, World");
-        System.out.println("Bye, World");
     }
 }
